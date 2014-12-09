1121f1790da708c5a240859a9fde7c26236baa2b
Maxim Parygin <developer.mak@gmail.com>
2014-12-09 18:29:01 +0600

    Daily commit

---
+++ test/Test_array.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * arrays test
+ */
+public class Test_array {
+
+    public static void set(int[] a, int index, int value) {
+        a[index] = value;
+    }
+
+    public static int get(int[] a, int index) {
+        return a[index];
+    }
+
+}
